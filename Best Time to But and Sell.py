5fde571cfa488c11bb2b00e2d43eb86b9ec05b03
Hementh-BV <107341439+Hementh-BV@users.noreply.github.com>
2022-09-14 09:47:21 +0530

    Create Best Time to But and Sell.py

---
+++ Best Time to But and Sell.py
@@ -0,0 +1,23 @@
+def max_profit(prices: list, days: int) -> int:
+    profit = 0
+
+    for i in range(1, days):
+
+        # checks if elements are adjacent and in increasing order
+        if prices[i] > prices[i - 1]:
+            # difference added to 'profit'
+            profit += prices[i] - prices[i - 1]
+
+    return profit
+
+
+# Driver Code
+if __name__ == '__main__':
+    # stock prices on consecutive days
+    prices = [100, 180, 260, 310, 40, 535, 695]
+
+    # function call
+    profit = max_profit(prices, len(prices))
+    print(profit)
+
+#  OUTPUT 865
